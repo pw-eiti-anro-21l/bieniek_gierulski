da9adf2f909f21ccbea099aaf8f685aa2d68923c
kbieniekpw <krzys.byniek@gmail.com>
2021-04-12 11:39:36 +0200

    lab_3

---
+++ lab3_essential/setup.py
@@ -0,0 +1,33 @@
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
+from setuptools import setup
+
+package_name = 'lab3_essential'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('urdf/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='krzysiek',
+    maintainer_email='krzys.byniek@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'state_publisher = lab3_essential.state_publisher:main'
+        ],
+    },
+)
